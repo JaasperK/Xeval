4dc2a766330e90b28b5380459370432f92fc9751
JaasperK <jasper.l.koehn@gmail.com>
2024-08-30 14:10:03 +0200

    formatting

--- src/path_getter.rs
+++ src/path_getter.rs
@@ -2,9 +2,5 @@ use std::path::PathBuf;
 use rfd::FileDialog;
 
 pub fn get_path() -> Option<PathBuf> {
-    if let Some(path) = FileDialog::new().pick_file() {
-        Some(path)
-    } else {
-        None
-    }
+    FileDialog::new().pick_file()
 }
